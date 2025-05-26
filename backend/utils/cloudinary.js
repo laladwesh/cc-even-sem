fbf30d1dc3f806d67ede56fb79b36e7e7b85921e
Avinash Gupta <guptaavinash302@gmail.com>
2025-05-26 11:39:56 +0530

    added cloudinary upload functionality to course creation and updates

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,7 +1,4 @@
 const cloudinary = require('cloudinary').v2;
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -10,6 +7,3 @@ cloudinary.config({
 });
 
 module.exports = cloudinary;
-// This code configures the Cloudinary SDK with your credentials.
-// It allows you to upload, manage, and manipulate images and videos in your application.
-// The `cloudinary` object can be used to perform various operations like uploading images, transforming them, etc.
