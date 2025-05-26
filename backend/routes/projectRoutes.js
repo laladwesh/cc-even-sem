64fdff845b07dad3906504bcc25409e5acfdfb6a
Avinash Gupta <guptaavinash302@gmail.com>
2025-05-26 22:47:15 +0530

    modified projectRoutes.js to include new endpoints for project management

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -2,13 +2,17 @@ const express = require('express');
 const router = express.Router();
 const projectController = require('../controllers/projectController');
 const { protect } = require('../middleware/authMiddleware');
+const { ClerkExpressRequireAuth } = require('@clerk/clerk-sdk-node');
+const { ensureAdmin } = require('../middleware/auth');
 
-router.get('/my', protect, projectController.getMyProjects);
-router.post('/', protect, projectController.createProject);
-router.put('/:id', protect, projectController.updateProject);
-router.delete('/:id', protect, projectController.deleteProject);
+router.get('/my', ClerkExpressRequireAuth(), projectController.getMyProjects);
+router.patch('/:id', ClerkExpressRequireAuth(), ensureAdmin ,projectController.editProjectAdmin);
+router.get('/admin', ClerkExpressRequireAuth(), ensureAdmin, projectController.getAllProjectsAdmin);
+router.post('/', ClerkExpressRequireAuth() ,ensureAdmin , projectController.createProject);
+// router.put('/:id', protect, projectController.updateProject);
+// router.delete('/:id', protect, projectController.deleteProject);
 
-router.post('/:id/assign', protect, projectController.assignProject);
-router.put('/:id/progress', protect, projectController.updateProgress);
+// router.post('/:id/assign', protect, projectController.assignProject);
+// router.put('/:id/progress', protect, projectController.updateProgress);
 
 module.exports = router;
