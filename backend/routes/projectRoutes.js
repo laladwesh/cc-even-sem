4745d86fec0e48e4b3e65990f19a840f2adbb963
Avinash Gupta <guptaavinash302@gmail.com>
2025-05-27 11:28:24 +0530

    modifed routes

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -9,6 +9,11 @@ router.get('/my', ClerkExpressRequireAuth(), projectController.getMyProjects);
 router.patch('/:id', ClerkExpressRequireAuth(), ensureAdmin ,projectController.editProjectAdmin);
 router.get('/admin', ClerkExpressRequireAuth(), ensureAdmin, projectController.getAllProjectsAdmin);
 router.post('/', ClerkExpressRequireAuth() ,ensureAdmin , projectController.createProject);
+router.patch(
+  '/:id/complete',
+  ClerkExpressRequireAuth(),   // custom middleware that checks req.auth.userId or admin
+  projectController.completeProject
+);
 // router.put('/:id', protect, projectController.updateProject);
 // router.delete('/:id', protect, projectController.deleteProject);
 
