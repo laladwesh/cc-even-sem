3a48a5021618f4e5aab7ecfa564454acb6ddfab5
Avinash Gupta <guptaavinash302@gmail.com>
2025-05-27 11:27:22 +0530

    modified Badge model to include a new badge for completing the course

--- backend/models/Badge.js
+++ backend/models/Badge.js
@@ -1,39 +1,9 @@
-const mongoose = require('mongoose');
+const { default: mongoose } = require("mongoose");
 
+// in models/Badge.js
 const badgeSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  type: {
-    type: String,
-    enum: ['course', 'project', 'custom'],
-    required: true,
-  },
-  description: {
-    type: String,
-    default: '',
-  },
-  associatedId: {
-    type: mongoose.Schema.Types.ObjectId,
-    refPath: 'typeRef',
-  },
-  typeRef: {
-    type: String,
-    enum: ['Course', 'Project'], // Refers to the model name for associatedId
-  },
-  iconUrl: {
-    type: String,
-    default: '', // Can be used with Cloudinary for custom icons
-  },
-  points: {
-    type: Number,
-    default: 50, // Default reward points per badge
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  name:       String,
+  description:String,
+  tokenValue: Number,    // how many “bonus cents” this badge is worth
 });
-
 module.exports = mongoose.model('Badge', badgeSchema);
