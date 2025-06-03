80ddf3e28608ddfa59369837734325932b7f2a29
arunika11 <arunikanaskar11@gmail.com>
2025-06-03 11:57:39 +0530

    model cjhanges

--- backend/models/Badge.js
+++ backend/models/Badge.js
@@ -1,9 +1,8 @@
 const { default: mongoose } = require("mongoose");
 
-// in models/Badge.js
 const badgeSchema = new mongoose.Schema({
   name:       String,
   description:String,
-  tokenValue: Number,    // how many “bonus cents” this badge is worth
+  tokenValue: Number, 
 });
 module.exports = mongoose.model('Badge', badgeSchema);
